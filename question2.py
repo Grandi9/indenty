a29f7ac356a97733ef7569427303565a89ed9d9d
Grandi9 <gsashrith@gmail.com>
2019-01-15 19:57:25 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,14 +1,10 @@
-import numpy as np 
 
-a = np.random.randint(1000,size=(5,4)) 
-print(a)
 
-rows = a.shape[0]
-cols = a.shape[1]
+import numpy as np
 
-for i in range(0, rows):
-    for j in range(0, cols):
-        if (a[i,j]>50)
-        	a[i,j]=100
+nums = np.random.rand(4,3)
+print(nums)
+#input check
 
-print(a)
+#output here
+print np.where(nums > 0.2, 0, nums)
