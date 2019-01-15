08c9f63b8bbed36694cb3ea13ca09342dcb2fa3b
Grandi9 <gsashrith@gmail.com>
2019-01-15 20:28:31 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -7,12 +7,7 @@ print(a)
 N = 10
 
 b=np.random.choice(a=[False, True], size=(N))  
-print(b)
 
-ans=np.zeros
-
-for j in b:
-  if(j=='False'):
-    ans = np.append(ans, a[j])
-
-print(ans)
+print(b) 
+result = a[b,...] 
+print(result) 
