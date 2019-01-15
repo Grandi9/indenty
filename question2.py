901b4c42db15c0306f17e0aa368fcb7bc39db837
Grandi9 <gsashrith@gmail.com>
2019-01-15 19:51:11 +0530

    Create question2.py

---
+++ question2.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+a = np.random.randint(1000,size=(5,4)) 
+print(a)
+
+rows = a.shape[0]
+cols = a.shape[1]
+
+for i in range(0, rows):
+    for j in range(0, cols):
+        if (a[i,j]>50)
+        	a[i,j]=100
+
+print(a)
